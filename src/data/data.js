41a5f62d20e87cda32e26e4286a3ff99e36cd7f2
fredkamm <fred.kamm95@gmail.com>
2022-10-10 15:35:09 -0400

    data file added

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import AnimeBlog from '../assets/projects/AnimeBlog.png'
+import AnimeList from '../assets/projects/AnimeList.png'
+import Password from '../assets/projects/PasswordGen.png'
+import Scheduler from '../assets/projects/scheduler.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:AnimeBlog,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:AnimeList,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:Password,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:Scheduler,
+        github:"",
+        live:"",
+    },
+
+
+]
